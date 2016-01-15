6715ea295dcbef58a74abf9d4ec983006ef2f3f0
Hiroyuki Okamura <okamu@rel.hiroshima-u.ac.jp>
2016-01-15 17:29:11 +0900

    add a function to compute KL in phfit.density

--- mapfit/R/fit_phfit.R
+++ mapfit/R/fit_phfit.R
@@ -32,13 +32,16 @@ phfit.group <- function(ph, counts, breaks, intervals, instant,
     })
 }
 
-phfit.density <- function(ph, f, deformula = zero.to.inf, weight.zero = .Machine$double.eps,
-  weight.reltol = sqrt(.Machine$double.eps),
-  method = c("all", "increment"), lbound = 1, ubound = NULL, 
-  control = list(), verbose = list(), ...) {
-  x <- deformula.weight(f, deformula, weight.zero, weight.reltol, ...)
-  data <- phfit.time.data.frame(time=x$x, weights=x$weights)
-  switch(class(ph),
+phfit.density <- function(ph, f,
+    method = c("all", "increment"), lbound = 1, ubound = NULL, 
+    deformula = deformula.zeroinf, weight.zero = 1.0e-12,
+    weight.reltol = 1.0e-8, start.divisions = 8, max.iter = 12,
+    control = list(), verbose = list(), ...) {
+  x <- deformula(f, ..., zero.eps = weight.zero, rel.tol = weight.reltol, start.divisions = start.divisions,
+    max.iter = max.iter)
+  ll <- x$h * sum(x$w * log(f(x$x, ...)))
+  data <- phfit.time.data.frame(time=x$x, weights=x$w)
+  res <- switch(class(ph),
     "ph"=phfit.gen(ph=ph, data=data, control=control, verbose=verbose, ...),
     "cf1"=phfit.cf1(ph=ph, data=data, control=control, verbose=verbose, ...),
     "herlang"={
@@ -49,4 +52,5 @@ phfit.density <- function(ph, f, deformula = zero.to.inf, weight.zero = .Machine
       phfit.herlang(phsize=phsize, data=data, method=method, lbound=lbound, ubound=ubound,
         control=control, verbose=verbose, ...)
     })
+  c(res, list(KL=ll - res$llf * x$h))
 }
